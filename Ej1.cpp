7a830de2728bb76a2f6b30cf926d28fa5778bf47
Aguscrack20 <aguspalaciotablet@gmail.com>
2026-06-16 17:32:12 -0300

    Cambio

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int potenciarecursividad(int base, int exponente)
+{
 
-    cout << "Examen Final Prog 3" << endl;
+    int resultado;
+
+    if (exponente == 0)
+    {
+        return 1;
+    }
+    else
+    {
+
+        resultado = base * potenciarecursividad(base, exponente - 1);
+
+        return resultado;
+    }
+}
+
+int main()
+{
+
+    cout << "Ejercicio 1" << endl;
+    int ba;
+    int ex;
+    int resultado;
+    cout << "Ingrese un numero " << endl;
+    cin >> ba;
+    cout << "ingrese hasta que expoenente quiere la recursividad " << endl;
+    cin >> ex;
+
+    resultado = potenciarecursividad(ba, ex);
+
+    cout << "Resultado " << resultado << endl;
 
     return 0;
 }
