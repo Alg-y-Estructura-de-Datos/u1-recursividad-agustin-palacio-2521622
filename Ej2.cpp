7601292e95838ad0714d8a9f23ef666bb0af1dd3
Aguscrack20 <aguspalaciotablet@gmail.com>
2026-06-17 15:12:46 -0300

    Cambios

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int sumarecursiva(int a)
+{
+    int suma_anterior;
 
-    cout << "Examen Final Prog 3" << endl;
+    if (a == 1)
+    {
+        cout<<"1";  //para imprimirlo por defecto
+        return 1;
+    }
+    else
+    {
+
+        suma_anterior=sumarecursiva(a-1);   C
+
+        cout<<" + "<<a;
+
+        return suma_anterior + a;
+        
+    }
+}
+
+int main()
+{
+    int su;
+    int resultado;
+    cout << "Ejercicio 2 " << endl;
+
+    cout << "Ingrese un numero " << endl;
+    cin >> su;
+    cout<<"El proceso de la suma es ";
+    resultado = sumarecursiva(su);
+
+    cout << "El resultado de la suma es " <<resultado<< endl;
 
     return 0;
 }
--- Ej3.cpp
+++ Ej3.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
+void mostrarInvertido(int numero, int& invertido) { //n = 123 invertido = 0
+
+    /*Condición Base*/
+    if (numero == 0) {
+        return;
+    } else {
+        /*Código necesario*/
+        invertido = invertido * 10 + numero % 10;       //invertido = 0*10 + 123 % 10 = 0+3 = 3
+        //invertido = 3*10 + 12%10 = 30 + 2 = 32
+        // invertido = 32*10 + 1%10 = 320 + 1 = 321
+        /*Recursividad y Terminación*/
+        mostrarInvertido(numero / 10, invertido); // return;
+    }
+}
+
 int main() {
+    int numero = 123;
+    cout << "Numero original: " << numero << endl;
 
-    cout << "Examen Final Prog 3" << endl;
+    if (numero < 0) {
+        cout << "Error: Número negativo no permitido." << endl;
+        return 1; // Salir del programa con error
+    }
 
+    int resultado = 0;
+    mostrarInvertido(numero, resultado);
+
+    cout << "Numero invertido: " << resultado << endl;
     return 0;
 }
